bcf22b49d7cfe4ea88b6f7d755a9e14537fe5f20
Nathan Cheek <myself@nathancheek.com>
2013-03-10 00:01:19 -0500

    Add reboot

--- iftttcontrol.sh
+++ iftttcontrol.sh
@@ -4,11 +4,19 @@
 IFTTTLOCATION="/home/nathancheek/Dropbox/IFTTT"
 LOGLOCATION="/home/nathancheek/IFTTT.log"
 SLEEP="sleep"
+REBOOT="reboot"
 Date=$(date +"%D %T")
 
 if [ -s $IFTTTLOCATION/$SLEEP.txt ]; then
 rm $IFTTTLOCATION/$SLEEP.txt
 echo "Going to sleep at $Date" >> $LOGLOCATION
 pm-suspend
-fi
 exit 1
+fi
+
+if [ -s $IFTTTLOCATION/$REBOOT.txt ]; then
+rm $IFTTTLOCATION/$REBOOT.txt
+echo "Rebooting at $Date" >> $LOGLOCATION
+reboot
+exit 1
+fi
