0b4a70d878d19b23472d597af7c3de4e361d33a7
Nathan Cheek <myself@nathancheek.com>
2013-03-21 09:30:57 -0400

    Add Shutdown and fix exit

--- iftttcontrol.sh
+++ iftttcontrol.sh
@@ -1,22 +1,29 @@
 #/bin/sh
 #This script checks for a Dropbox-synced file and then runs a preconfigured command accordingly
 
-IFTTTLOCATION="/home/nathancheek/Dropbox/IFTTT"
-LOGLOCATION="/home/nathancheek/IFTTT.log"
+IFTTTLOCATION="/home/username/Dropbox/IFTTT"
+LOGLOCATION="/home/username/IFTTT.log"
 SLEEP="sleep"
 REBOOT="reboot"
+SHUTDOWN="shutdown"
 Date=$(date +"%D %T")
 
 if [ -s $IFTTTLOCATION/$SLEEP.txt ]; then
 rm $IFTTTLOCATION/$SLEEP.txt
 echo "Going to sleep at $Date" >> $LOGLOCATION
 pm-suspend
-exit 1
+exit 0
 fi
-
 if [ -s $IFTTTLOCATION/$REBOOT.txt ]; then
 rm $IFTTTLOCATION/$REBOOT.txt
 echo "Rebooting at $Date" >> $LOGLOCATION
 reboot
-exit 1
+exit 0
 fi
+if [ -s $IFTTTLOCATION/$SHUTDOWN.txt ]; then
+rm $IFTTTLOCATION/$SHUTDOWN.txt
+echo "Shutting down at $Date" >> $LOGLOCATION
+shutdown now
+exit 0
+fi
+exit 0
